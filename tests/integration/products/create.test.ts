586e3c6f68a8275c438ca13d94e2f1212d0b016f
Mario Teixeira <mariomarcio12324@gmail.com>
2023-09-14 12:58:18 -0300

    Finalizado req 01 com os testes.

--- tests/integration/products/create.test.ts
+++ tests/integration/products/create.test.ts
@@ -1,10 +1,22 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import app from '../../../src/app';
+import productsMock from '../../mocks/products.mock';
+import { Product } from '../../../src/types/Product';
+import ProductModel from '../../../src/database/models/product.model';
+
 
 chai.use(chaiHttp);
 
 describe('POST /products', function () { 
   beforeEach(function () { sinon.restore(); });
-
+  it('should return status 201 and the created product', async function () {
+    const product = productsMock.validateProduct;
+    const response = ProductModel.build(productsMock.valideResponse);
+    sinon.stub(ProductModel, 'create').resolves(response);
+    const httpResponse = await chai.request(app).post('/products').send(product);
+    expect(httpResponse.status).to.equal(201);
+  });
 });
+
---
+++ tests/mocks/products.mock.ts
@@ -0,0 +1,17 @@
+const validateProduct= {
+  name: 'Martelo de Thor',
+  price: '30 peças de ouro',
+  orderId: '5'
+}
+
+const valideResponse = {
+  id: 1,
+  name: 'Martelo de Thor',
+  price: '30 peças de ouro',
+  orderId: 4
+}
+
+export default {
+  validateProduct,
+  valideResponse
+}
